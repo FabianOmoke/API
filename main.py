70f7fa4e9313f51d3e8e466f0f2a5e47ddd81863
Fabian_Omoke <f.ombui@blue-planet-ecosystems.com>
2022-07-22 11:57:36 +0200

    checked

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from unicodedata import category
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 import uvicorn
 from pydantic import BaseModel
 from random import randrange
@@ -24,6 +24,11 @@ def find_posts(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 
 @app.get("/")
 def read_root():
@@ -33,22 +38,25 @@ def read_root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Post):
     post_dict = new_post.dict()
     post_dict['id']= randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
 
-@app.get("/posts/latest")
-def get_latest_post():
-    latest_post = my_posts[-1]
-    return {"detail": latest_post}
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_posts(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} was not found")
     return {"post details": post }
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    my_posts.pop(id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 if __name__ == "__main__":
